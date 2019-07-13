cdf7a75a745a8cecaa1d7d34c6870ffa00726f90
sepans <sepans@sepans.com>
2019-07-13 18:16:14 -0400

    migration add deletedAt

---
+++ src/server/migrations/20190713210649-response-add-deleted.js
@@ -0,0 +1,72 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    const migrations = []
+
+    migrations.push(queryInterface.addColumn(
+      'CandidateResponses',
+      'deleted',
+      {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+    ))    
+    migrations.push(queryInterface.addColumn(
+      'CandidateResponses',
+      'deletedAt',
+      {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+    ))
+    migrations.push(queryInterface.addColumn(
+      'UserResponses',
+      'deleted',
+      {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+    ))
+    migrations.push(queryInterface.addColumn(
+      'UserResponses',
+      'deletedAt',
+      {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+    ))    
+
+
+    return Promise.all(migrations)
+
+  },
+
+  down: (queryInterface, Sequelize) => {
+    const migrations = []
+
+    migrations.push(queryInterface.removeColumn(
+      'UserResponses',
+      'deleted',
+    ))
+
+    migrations.push(queryInterface.removeColumn(
+      'UserResponses',
+      'deletedAt',
+    ))
+
+    migrations.push(queryInterface.removeColumn(
+      'CandidateResponses',
+      'deleted',
+    ))
+
+    migrations.push(queryInterface.removeColumn(
+      'CandidateResponses',
+      'deletedAt',
+    ))
+
+    return Promise.all(migrations)
+  }
+};
