af215a9e91ada5bbacb8c1e55993a220783d1656
sepans <sepans@sepans.com>
2019-08-08 15:19:32 -0500

    CandidateList.spec.tsx working. CandidateInfo.spec.tsx has routing problem

--- src/components/CandidateInfo.spec.tsx
+++ src/components/CandidateInfo.spec.tsx
@@ -1,52 +1,83 @@
 import { createMockEnvironment, MockPayloadGenerator } from 'relay-test-utils'
-// import { render } from 'enzyme'
-import { ReactTestRenderer } from 'react-test-renderer'
+import reactTestRenderer from 'react-test-renderer'
 import React from 'react'
-// import { render } from 'enzyme'
 import { graphql, QueryRenderer } from 'react-relay'
-// import graphql from 'babel-plugin-relay/macro'
-// import { graphql } from 'relay-runtime'
 
 import CandidateInfo from './CandidateInfo'
-// import { CandidateInfo_candidate } from './__generated__/CandidateInfo_candidate.graphql'
 
 describe('CandidateInfo', () => {
   const environment = createMockEnvironment()
-  const renderer = ReactTestRenderer.create(
-    <CandidateInfo environment={environment} />
-  )
-  // const TestRenderer = () => {
-  //   return (
-  //     <QueryRenderer
-  //       environment={environment}
-  //       query={graphql`
-  //         query App_Candidate_Query($id: ID!) {
-  //           candidate(id: $id) {
-  //             ...CandidateInfo_candidate
-  //           }
-  //         }
-  //       `}
-  //       variables={{ id: 1 }}
-  //       render={({ error, props }) => {
-  //         console.log('PROPS', props, error)
-  //         if (props) {
-  //           return <CandidateInfo myData={props.myData} />
-  //         } else if (error) {
-  //           return error.message
-  //         }
-  //         return 'Loading...'
-  //       }}
-  //     />
-  //   )
-  // }
-  // const renderer = ReactTestRenderer.create(<TestRenderer />)
 
-  // environment.mock.resolveMostRecentOperation(operation => {
-  //   console.log('##### running ', operation)
-  //   MockPayloadGenerator.generate(operation)
-  // })
+  const TestRenderer = () => {
+    return (
+      <QueryRenderer
+        environment={environment}
+        query={graphql`
+          query CandidateInfo_Query($id: ID!) @relay_test_operation {
+            candidate(id: $id) {
+              ...CandidateInfo_candidate
+            }
+          }
+        `}
+        variables={{ id: 1 }}
+        render={({ error, props }) => {
+          if (props) {
+            return <CandidateInfo candidate={props.candidate} />
+          }
+          return 'Loading...'
+        }}
+      />
+    )
+  }
 
-  expect(renderer).toMatchInlineSnapshot()
-  // const wrapper = render(<TestRenderer />)
-  // console.log(wrapper.debug())
+  it('renders', () => {
+    const renderer = reactTestRenderer.create(<TestRenderer />)
+    environment.mock.resolveMostRecentOperation(operation =>
+      MockPayloadGenerator.generate(operation)
+    )
+    expect(renderer).toMatchInlineSnapshot(`
+      Array [
+        <div
+          className="sui-box sc-bwzfXH bQCxlf"
+        >
+          <div
+            className="sc-iAyFgw brrEtL"
+          />
+        </div>,
+        <span
+          className="sui-typo sc-kEYyzF cmIyuR"
+          style={Object {}}
+        >
+          &lt;mock-value-for-field-"name"&gt;
+        </span>,
+        <span
+          className="sui-typo sc-kEYyzF cmIyuR"
+          style={Object {}}
+        >
+          &lt;mock-value-for-field-"experience"&gt;
+        </span>,
+        <span
+          className="sui-typo sc-kEYyzF zcVTn"
+          style={Object {}}
+        >
+          state: 
+          &lt;mock-value-for-field-"state"&gt;
+        </span>,
+        <span
+          className="sui-typo sc-kEYyzF zcVTn"
+          style={Object {}}
+        >
+          age: 
+          NaN
+        </span>,
+        <span
+          className="sui-typo sc-kEYyzF zcVTn"
+          style={Object {}}
+        >
+          Place of birth: 
+          &lt;mock-value-for-field-"pob"&gt;
+        </span>,
+      ]
+    `)
+  })
 })
--- src/pages/CandidateList.spec.tsx
+++ src/pages/CandidateList.spec.tsx
@@ -1,54 +1,44 @@
 import { createMockEnvironment, MockPayloadGenerator } from 'relay-test-utils'
-// import { render } from 'enzyme'
-import { ReactTestRenderer } from 'react-test-renderer'
-import { render } from 'enzyme'
+import reactTestRenderer from 'react-test-renderer'
+import React from 'react'
+
 import { graphql, QueryRenderer } from 'react-relay'
-// import graphql from 'babel-plugin-relay/macro'
-// import { graphql } from 'relay-runtime'
 
 import CandidateList from './CandidateList'
-// import { CandidateInfo_candidate } from './__generated__/CandidateInfo_candidate.graphql'
 
 describe('CandidateList', () => {
   const environment = createMockEnvironment()
-  // const renderer = ReactTestRenderer.create(
-  //   <CandidateInfo environment={environment} />,
-  // );
   const TestRenderer = () => {
-    debugger
     return (
       <QueryRenderer
         environment={environment}
         query={graphql`
-          query App_Candidates_Query {
+          query CandidateList_Query {
             candidates {
               ...CandidateList_candidates
             }
           }
         `}
         variables={{}}
-        // render={({ error, props }) => {
-        //   console.log('PROPS', props, error)
-        //   if (props) {
-        //     return <CandidateList myData={props.myData} />
-        //   } else if (error) {
-        //     return error.message
-        //   }
-        //   return 'Loading...'
-        // }}
-        render={({ props }) => {
-          return props ? <CandidateList {...props} /> : <div>loading</div>
+        render={({ error, props }) => {
+          if (props) {
+            return <CandidateList candidates={props.candidates} />
+          }
+          return 'Loading...'
         }}
       />
     )
   }
-  // const renderer = ReactTestRenderer.create(<TestRenderer />)
-  environment.mock.resolveMostRecentOperation(operation => {
-    console.log('##### running ', operation)
-    MockPayloadGenerator.generate(operation)
+
+  it('renders', () => {
+    const renderer = reactTestRenderer.create(<TestRenderer />)
+    environment.mock.resolveMostRecentOperation(operation =>
+      MockPayloadGenerator.generate(operation, {
+        ID(_, generateId) {
+          return `candidate-id-${generateId()}`
+        },
+      })
+    )
+    expect(renderer).toMatchInlineSnapshot(`null`)
   })
-  console.log('RENDERING')
-  // expect(renderer).toMatchInlineSnapshot()
-  const wrapper = render(<TestRenderer />)
-  console.log(wrapper.debug())
 })
